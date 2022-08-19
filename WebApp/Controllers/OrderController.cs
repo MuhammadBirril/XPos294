f8733b4befd09f4f78b14e82858429d64174b20c
rickysadewa007 <rickysadewa007@gmail.com>
2022-08-19 13:36:37 +0700

    Order init

--- WebApp/Controllers/OrderController.cs
+++ WebApp/Controllers/OrderController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Net.Http;
+using System.Linq;
 using System.Threading.Tasks;
 using ViewModel;
 using WebApp.Services;
@@ -14,11 +13,11 @@ namespace WebApp.Controllers
     [Authorize(Roles = "Administrator,Order")]
     public class OrderController : Controller
     {
-
         private readonly ProductService proSrv;
-        public OrderController (IConfiguration configuration)
+
+        public OrderController(IConfiguration configuration)
         {
-            proSrv = new ProductService (configuration);
+            proSrv = new ProductService(configuration);
         }
         public IActionResult Index()
         {
@@ -28,7 +27,7 @@ namespace WebApp.Controllers
         public async Task<IActionResult> ProductList()
         {
             List<ProductViewModel> list = await proSrv.GetAll();
-            return PartialView("_ProductList");
+            return PartialView("_ProductList", list);
         }
     }
 }
